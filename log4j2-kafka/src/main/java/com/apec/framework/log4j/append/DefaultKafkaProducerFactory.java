5222474c5b37a95c9438c9da344a8e31d1569f06
王飞 <wangfei@ap-ec.cn>
2018-09-29 17:42:41 +0800

    log4j

---
+++ log4j2-kafka/src/main/java/com/apec/framework/log4j/append/DefaultKafkaProducerFactory.java
@@ -0,0 +1,15 @@
+package com.apec.framework.log4j.append;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+
+public class DefaultKafkaProducerFactory implements KafkaProducerFactory {
+
+	@Override
+	public Producer<byte[], byte[]> newKafkaProducer(final Properties config) {
+		return new KafkaProducer<>(config);
+	}
+
+}
---
+++ log4j2-kafka/src/main/java/com/apec/framework/log4j/append/KafkaProducerFactory.java
@@ -0,0 +1,11 @@
+package com.apec.framework.log4j.append;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.Producer;
+
+public interface KafkaProducerFactory {
+
+    Producer<byte[], byte[]> newKafkaProducer(Properties config);
+
+}
